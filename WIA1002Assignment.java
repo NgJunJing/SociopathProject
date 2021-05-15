63dccbcd4133333882feefc5c804ff20051329b7
NgJunJing <80374599+NgJunJing@users.noreply.github.com>
2021-05-15 13:20:47 +0800

    Add files via upload

---
+++ WIA1002Assignment.java
@@ -0,0 +1,11 @@
+
+package wia1002.assignment;
+
+public class WIA1002Assignment {
+
+    // Empty code file
+    public static void main(String[] args) {
+        System.out.println("Edit");
+    }
+    
+}
