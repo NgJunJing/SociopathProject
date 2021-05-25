be138445b90b908b07dc57440edcbd8067dee32a
NgJunJing <80374599+NgJunJing@users.noreply.github.com>
2021-05-25 23:02:16 +0800

    Update of event 3 after referring to FAQ

--- main.java
+++ main.java
@@ -24,7 +24,7 @@ import java.util.Scanner;
 public class main {
     
     static final String DBURL = // your database connection url
-            "jdbc:sqlserver://LAPTOP-NPQJUSVA\\SQLEXPRESS:49671;databaseName=master"; 
+            "jdbc:sqlserver://LAPTOP-NPQJUSVA\\SQLEXPRESS:49672;databaseName=master"; 
     static final String DBDRV = // your database connection driver
             "com.microsoft.sqlserver.jdbc.SQLServerDriver";
     static final String DBUSER = "sa"; // your user name
@@ -252,10 +252,56 @@ public class main {
         Scanner sc= new Scanner(System.in); 
         System.out.println("EVENT 3: Your Road to Glory");
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        // display all available choices
+        // ask for user input for today's lunch time and duration
+        int yourStartTime, yourEndTime;
+        System.out.println("Provide your lunch starting time and planned duration of lunch today");
+        do {
+            System.out.println("Lunch start time(1100-1400):");
+            try {
+                String in = sc.next();
+                yourStartTime = Integer.parseInt(in);
+                sc.nextLine();
+            } catch (NumberFormatException e) { //check is the input an integer
+                System.out.println("Invalid input");
+                continue;
+            }
+            if (yourStartTime>1400 || yourStartTime<1100) {
+                System.out.println("Invalid lunch time. Lunch time should be between 1100 to 1400");
+                continue;
+            } else if (yourStartTime%100 > 59) {
+                System.out.println("Invalid time input");
+                continue;
+            }
+            break;
+        } while(true);
+        do {
+            System.out.println("Lunch duration(mins):");
+            int duration;
+            try {
+                String in = sc.next();
+                duration = Integer.parseInt(in);
+                sc.nextLine();
+            } catch (NumberFormatException e) { //check is the input an integer
+                System.out.println("Invalid input");
+                continue;
+            }
+            if (duration>59 || duration<5) {
+                System.out.println("Invalid lunch duration. Lunch time should be between 6 to 59");
+                continue;
+            }
+            int hour = yourStartTime/100;
+            int mins = yourStartTime%100;
+            mins = mins + duration;
+            if (mins >= 60) {
+                hour += (mins/60);
+                mins = (mins%60);
+            }
+            yourEndTime = hour*100 + mins;
+            break;
+        } while(true);
         
         // store all lunch time, lunch period, and dive rate info except for yourID
-        ArrayList<Integer[]> startTimeEndTimeCollection = new ArrayList<>();
+        ArrayList<Integer[]> validIntersection = new ArrayList<>();
         // display all possible choice of person
         try {
             con = DriverManager.getConnection(DBURL, DBUSER, DBPASSWD);
@@ -265,170 +311,121 @@ public class main {
             "where Person.ID !="+yourID+';';
             rs = st.executeQuery(getInfo);
             
-            // display the available students for selection
-            System.out.println("Available student");
-            System.out.printf("%-6s%14s%14s%14s\n", "ID", "diving_rate", "lunch_time", "lunch_period");
-            while (rs.next()) {
-                int ID = rs.getInt("ID");
-                int diveRate = rs.getInt("diving_rate");
-                int lunchtime = Integer.parseInt(rs.getString("lunch_time"));
-                int lunchperiod = rs.getInt("lunch_period");
-                System.out.printf("%-6d%12d%12d%12d\n", ID, diveRate, lunchtime, lunchperiod);
+            while(rs.next()) {
+                int id = rs.getInt("ID");
+                int dive = rs.getInt("diving_rate");
+                int startTime = Integer.parseInt(rs.getString("lunch_time"));
+                int lunchPeriod = rs.getInt("lunch_period");
+                
+                // calculate lunch end time
+                int hour = startTime/100;
+                int mins = startTime%100;
+                mins = mins + lunchPeriod;
+                if (mins >= 60) {
+                    hour += (mins/60);
+                    mins = (mins%60);
+                }
+                int endTime = hour*100 + mins;
+                
+                //store
+                if ((endTime>yourStartTime && endTime<yourEndTime) || (startTime<yourEndTime && startTime>yourStartTime)) {
+                    validIntersection.add(new Integer[] {id, startTime, endTime, dive});
+                }
             }
             
-            System.out.println("Select the people you are interested to have lunch with, by entering their respective ID");
-            System.out.println("[ONE ID PER INPUT][Enter '0' to quit selection]"); // test 2 inputs at once
-            int input;
-            ArrayList<Integer> checkID = new ArrayList<>();
-            do {
-                System.out.print("Input: ");
-                System.out.println("");
-                try {
-                    String in = sc.next();
-                    input = Integer.parseInt(in);
-                    sc.nextLine();
-                } catch (NumberFormatException e) { //check is the input an integer
-                    System.out.println("Invalid input");
-                    continue;
-                } 
-                // check if input is 0
-                if (input == 0) {
-                    System.out.println("Selection exited.\n");
-                    break;
-                }
-                // check if input equals to the given ID
-                if (input == yourID) {
-                    System.out.println("This is you.");
-                    continue;
-                }
-                // check if input ID has been selected
-                if (checkID.contains(input)) {
-                    System.out.println("This ID has been selected");
-                    continue;
-                }
-                // check the input ID
-                String search = "Select *\n" +
-                "from db_owner.Persons Person\n" +
-                "where Person.ID ="+input+';';
-                rs = st.executeQuery(search);
-                
-                if (rs.next()) { //if ID is found
-                    int id = rs.getInt("ID");
-                    int startTime = Integer.parseInt(rs.getString("lunch_time"));
-                    int lunchPeriod = rs.getInt("lunch_period");
-
-                    // calculate lunch end time
-                    int hour = startTime/100;
-                    int mins = startTime%100;
-                    mins = mins + lunchPeriod;
-                    if (mins >= 60) {
-                        hour += (mins/60);
-                        mins = (mins%60);
-                    }
-                    int endTime = hour*100 + mins;
-                    
-                    //store
-                    Integer[] data = {id, startTime, endTime};
-                    checkID.add(id);
-                    startTimeEndTimeCollection.add(data);
-                    System.out.println("ID recorded");
-                    
-                } else { // if ID not available
-                    System.out.println("ID not found");
-                }
-            } while (true);
         } catch (SQLException ex) {
             ex.printStackTrace();
             return false;
         }
-        // Exit the program if no one is being selected
-        if (startTimeEndTimeCollection.isEmpty()) {
-            System.out.println("You have not selected anyone.");
-            System.out.println("Event 3 ended.");
+        if(validIntersection.isEmpty()) {
+            System.out.println("None of the average lunch time intersects with your lunch time today");
+            System.out.println("\nEvent 3 ended.");
             System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
             return true;
         }
-        
-        // display the selected list of students
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        System.out.println("Selected students");
-        System.out.println("-----------------------------------");
-        System.out.printf("| %-6s|%12s|%12s|\n", "ID", "start_time", "end_time");
-        System.out.println("-----------------------------------");
-        startTimeEndTimeCollection = sortStartTimeEndTime(startTimeEndTimeCollection); // sort the list
+        System.out.println("These are the students with their average lunch time \n"
+               + "that intersect with your today's lunch time");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("------------------------------------------------");
+        System.out.printf("| %-6s|%12s|%12s|%12s|\n", "ID", "start_time", "end_time", "dive_rate");
+        System.out.println("------------------------------------------------");
         // display the list
-        for (int i = 0; i<startTimeEndTimeCollection.size() ; i++) {
-            Integer[] arr = startTimeEndTimeCollection.get(i);
-            System.out.printf("| %-6d|%12d|%12d|\n", arr[0], arr[1], arr[2]);
+        for (int i = 0; i<validIntersection.size() ; i++) {
+            Integer[] arr = validIntersection.get(i);
+            System.out.printf("| %-6d|%12d|%12d|%12d|\n", arr[0], arr[1], arr[2], arr[3]);
         }
-        System.out.println("-----------------------------------");
-        
-        // arrayList to store final ID for lunch
-        // stack to store combinations of time slots
-        ArrayList<Integer[]> IDForLunch = new ArrayList<>();
-        LinkedList<Integer[]> countStack = new LinkedList<>();
-        int max = 0;
-        for (int i=0; i<startTimeEndTimeCollection.size() ; i++) {
+        System.out.println("------------------------------------------------");
             
+        // calculate the maximum reputation that can be gotten in one day
+        validIntersection = sortStartTimeEndTime(validIntersection); // sort the list
+        int max = 0; // maximum reputation
+        ArrayList<ArrayList<Integer[]>> possibleCombination = new ArrayList<>(); // list to store all possible combinations
+        for (int i=0; i<validIntersection.size() ; i++) {
+            ArrayList<Integer[]> eachCombination = new ArrayList<>(); // to store each combination
             // try each combinations
-            for (int j = i; j<startTimeEndTimeCollection.size() ; j++) {
-                if (countStack.isEmpty()) {
-                    countStack.push(startTimeEndTimeCollection.get(i));
+            for (int j = i, count = 0; j<validIntersection.size() ; j++) {
+                if (eachCombination.isEmpty()) {
+                    eachCombination.add(validIntersection.get(i));
+                    count++;
                 } else {
                     // if the start time of next person is later than the end time of previous person then can add
-                    if (startTimeEndTimeCollection.get(j)[1].compareTo(countStack.peek()[2])>0) { 
-                        countStack.push(startTimeEndTimeCollection.get(j));
+                    if (validIntersection.get(j)[1].compareTo(eachCombination.get(count-1)[2])>0) { 
+                        eachCombination.add(validIntersection.get(j));
+                        count++;
                     }
                 }
             }
-            
-            int count = countStack.size();
-            if (count > max) {
-                max=count;
-                if (!IDForLunch.isEmpty()) IDForLunch.clear();
-                
-                // store the new IDs
-                while (!countStack.isEmpty()) {
-                    IDForLunch.add(countStack.pop());
-                }
+            int counts = eachCombination.size();
+            if (counts > max) {
+                max=counts;
             } 
-            if(!countStack.isEmpty()) countStack.clear(); // clear the stack for next iteration
-            
+            possibleCombination.add(eachCombination); // store each combination into possible combination
         }
         System.out.println("The maximum number of reputation that you can get is " + max + "\n");
-        // display all the students that you will have lunch with
-        for (int i=IDForLunch.size()-1 ; i>=0 ; i--) {
-            System.out.println("You can have lunch with student, ID="+IDForLunch.get(i)[0]+" from "+IDForLunch.get(i)[1]+
-                    " to "+IDForLunch.get(i)[2]+" to increase your reputation relative to that student by 1");
+        // display all the possible combinations of student that you can have lunch with
+        System.out.println("All of the possible combinations of student that you can have lunch with");
+        for (int i=0 ; i<possibleCombination.size() ; i++) {
+            System.out.print((i+1) + " ");
+            for (int j=0; j<possibleCombination.get(i).size() ; j++) {
+                System.out.print(Arrays.toString(possibleCombination.get(i).get(j)));
+            }
+            System.out.println("");
         }
+        
+        // select one possible combination
+        System.out.println("Please select the combination that you would like to have lunch with. ['0' to quit selection]");
+        int selection = -1;
         do {
-            System.out.println("\nDo you want to have lunch with them?[yes/no]");
-            String in = sc.next();
-            sc.nextLine();
-            if (in.equals("no")) {
-                System.out.println("Event 3 ended.");
+            System.out.println("");
+            try {
+                String in = sc.next();
+                selection = Integer.parseInt(in);
+            } catch (NumberFormatException nfe) {
+                System.out.println("Invalid input");
+                continue;
+            }
+            if (selection == 0) {
+                System.out.println("You did not choose any combination");
+                System.out.println("\nEvent 3 ended.");
                 System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
                 System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
                 return true;
             }
-            else if (in.equals("yes")) {
-                System.out.println("Updating relationships");
-                break;
+            if (selection > possibleCombination.size() - 1) {
+                System.out.println("Invalid selection");
+                continue;
             }
-            System.out.println("Invalid input");
+            selection = selection - 1;
+            break;
         } while(true);
         
-        // update friend relationship and/or reputation of your ID relative to selected ID
-        for (int i=IDForLunch.size()-1 ; i>=0; i--) {
+        // update relationship reputation of your ID relative to selected ID
+        for (int i=possibleCombination.get(selection).size()-1 ; i>=0; i--) {
             // check whether the two persons were already connected
-            if (hasEdge(yourID, IDForLunch.get(i)[0])) {
-                if (!isFriend(yourID, IDForLunch.get(i)[0])) {// check whether the two persons were already friends, if not, add friend
-                    addFriend(yourID, IDForLunch.get(i)[0]);
-                }
-            } else { // no edge
-                if(addEdge(yourID, IDForLunch.get(i)[0])) // form edge
-                    addFriend(yourID, IDForLunch.get(i)[0]); // add friend
+            if (!hasEdge(yourID, possibleCombination.get(selection).get(i)[0])) {
+                addEdge(yourID, possibleCombination.get(selection).get(i)[0]);
             }
             
             try {
@@ -436,11 +433,11 @@ public class main {
                 con = DriverManager.getConnection(DBURL, DBUSER, DBPASSWD);
                 st = con.createStatement();
                 // get current rep
-                String getRep = "SELECT Person1.ID, Person2.ID, Person2.diving_rate, reputation \n" +
+                String getRep = "SELECT Person1.ID, Person2.ID, reputation \n" +
                 "FROM db_owner.Persons AS Person1, \n" +
                 "	 db_owner.Persons AS Person2,\n" +
                 "	 db_owner.Relationships AS Re\n" +
-                "WHERE MATCH(Person2-(Re)->Person1) AND Person1.ID ="+yourID+" AND Person2.ID ="+IDForLunch.get(i)[0]+';';
+                "WHERE MATCH(Person2-(Re)->Person1) AND Person1.ID ="+yourID+" AND Person2.ID ="+possibleCombination.get(selection).get(i)[0]+';';
                 rs = st.executeQuery(getRep);
                 if (rs.next()) {
                     currentRep = rs.getInt("reputation");
@@ -450,17 +447,17 @@ public class main {
                 String updateRep = "UPDATE db_owner.Relationships SET reputation = " +(currentRep+1)+" FROM db_owner.Persons AS Person1, \n" +
                 "	 db_owner.Persons AS Person2,\n" +
                 "	 db_owner.Relationships AS Re\n" + 
-                "WHERE MATCH(Person2-(Re)->Person1) AND Person1.ID =" +yourID+" AND Person2.ID = " +IDForLunch.get(i)[0]+';';
+                "WHERE MATCH(Person2-(Re)->Person1) AND Person1.ID =" +yourID+" AND Person2.ID = " +possibleCombination.get(selection).get(i)[0]+';';
                 st.execute(updateRep);
                 
                 // get updated rep
                 String selectRep = "SELECT Re.reputation \n" +
                 "From db_owner.Persons P1, db_owner.Persons P2, db_owner.Relationships Re\n" +
-                "Where match(P2-(Re)->P1) and P1.ID =" +yourID+"and P2.ID = " +IDForLunch.get(i)[0]+';';
+                "Where match(P2-(Re)->P1) and P1.ID =" +yourID+"and P2.ID = " +possibleCombination.get(selection).get(i)[0]+';';
                 rs = st.executeQuery(selectRep);
                 if (rs.next()) {
                     int newRep = rs.getInt("reputation");
-                    System.out.println("Your reputation relative to student, ID="+IDForLunch.get(i)[0]+
+                    System.out.println("Your reputation relative to student, ID="+possibleCombination.get(selection).get(i)[0]+
                         " increased from " + currentRep + " to " + newRep);
                 }
                 
